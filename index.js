b3bd6af5d33490ed48c98a1149cc9b05a31b830c
Grunet <sadikshahidain@gmail.com>
2020-07-21 21:04:35 -0500

    Initial usage of the email sending function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello");
+const { sendEmailsToRecipients } = require("digestible-wcag-email-sending");
+
+(async function () {
+  try {
+    const inputs = {};
+
+    await sendEmailsToRecipients(inputs);
+  } catch (error) {
+    console.error(error);
+  }
+})();
