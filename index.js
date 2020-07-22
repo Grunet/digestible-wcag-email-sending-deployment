454ab500e29b77236a26f55f333824aacd6e9c54
Grunet <sadikshahidain@gmail.com>
2020-07-21 21:53:46 -0500

    Introducing the interface to get the secret keys

--- index.js
+++ index.js
@@ -2,10 +2,28 @@ const { sendEmailsToRecipients } = require("digestible-wcag-email-sending");
 
 (async function () {
   try {
-    const inputs = {};
+    const { sendGridApiKey, senderEmail } = getSecrets();
+
+    const inputs = {
+      apiKeys: {
+        sendGrid: {
+          sendOnly: sendGridApiKey,
+        },
+      },
+      emails: {
+        sender: senderEmail,
+      },
+    };
 
     await sendEmailsToRecipients(inputs);
   } catch (error) {
     console.error(error);
   }
 })();
+
+function getSecrets() {
+  return {
+    sendGridApiKey: "",
+    senderEmail: "",
+  };
+}
